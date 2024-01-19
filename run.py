d858ca0601417b72261cae13d2b5d58183d18657
Kenarina <>
2024-01-19 17:24:31 +0000

    Create function to receive user's raw data.

--- run.py
+++ run.py
@@ -12,8 +12,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Water-Quality-Advisory')
 
-UserEntry = SHEET.worksheet('Raw_Data')
+def get_raw_data():
+    print("Please enter your raw data below.\n")
+    print("Please enter the date and your measurements in the following order:\n")
+    print("Day of the month (e.g. 1, 2, 3,...), pH, Lead, Cadmium, Zinc, Mercury, Arsenic, Chromium, Nickel.\n")
+    print("Please make sure your units are in mg/L.\n")
 
-UserData = UserEntry.get_all_values()
+    raw_data = input("Enter your raw data here:")
+    print(f"You have entered: {raw_data}")
 
-print(UserData)
+get_raw_data()
